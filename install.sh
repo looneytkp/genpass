9d0beaea09158b95f08443a0bae121a4ee8b7d1d
looneytkp <sgm4kv@gmail.com>
2017-12-09 11:24:47 +0000

    updated update parameters

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
 path=/usr/bin/mkpasswd
-if [ -e mkpasswd -a -e $path ]; then
+case $1 in
+"") if [ -e mkpasswd -a -e $path ]; then
 chmod 755 mkpasswd
 sudo rm $path && sudo cp mkpasswd $path
-echo "mkpasswd is installed"
 mkpasswd
+echo "mkpasswd is installed"
 elif [ -e mkpasswd -a ! -e $path ]; then
 chmod 755 mkpasswd
 sudo cp mkpasswd $path
-echo "installed"
+echo "mkpasswd is installed"
 mkpasswd
 else echo "cannot find mkpasswd"
-fi
+fi;;
+"--update") if [ -e mkpasswd -a -e $path ]; then
+chmod 755 mkpasswd
+sudo rm $path && sudo cp mkpasswd $path
+#echo "mkpasswd is installed"
+elif [ -e mkpasswd -a ! -e $path ]; then
+chmod 755 mkpasswd
+sudo cp mkpasswd $path
+#echo "mkpasswd is installed"
+else echo "cannot find mkpasswd"
+fi;;
+esac
