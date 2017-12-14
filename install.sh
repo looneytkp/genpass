a0628e39a2ff162865ab1749cabad2b20f321957
looneytkp <sgm4kv@gmail.com>
2017-12-14 15:47:53 +0000

    fix bugs

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@ set -e
 path=/usr/bin/mkpasswd
 case $1 in
 "") if [ -e mkpasswd -a -e $path ]; then
-chmod 755 mkpasswd
+#chmod 755 mkpasswd
 sudo rm $path && sudo cp mkpasswd $path
 mkpasswd
 echo "mkpasswd is installed"
 elif [ -e mkpasswd -a ! -e $path ]; then
-chmod 755 mkpasswd
+#chmod 755 mkpasswd
 sudo cp mkpasswd $path
 echo "mkpasswd is installed"
 mkpasswd
 else echo "cannot find mkpasswd"
 fi;;
 "--update") if [ -e ~/.configfile/mkpasswd -a -e $path ]; then
-chmod 755 ~/.configfile/mkpasswd
+#chmod 755 ~/.configfile/mkpasswd
 sudo rm $path && sudo cp ~/.configfile/mkpasswd $path
 #echo "mkpasswd is installed"
 elif [ ! -e ~/.configfile/mkpasswd -a -e $path ]; then
-chmod 755 ~/.configfile/mkpasswd
+#chmod 755 ~/.configfile/mkpasswd
 sudo cp ~/.configfile/mkpasswd $path
 else echo "cannot find mkpasswd"
 fi;;
