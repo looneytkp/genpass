aa8d92f2d129ae78bd2104712faec8b9e6122e9b
looneytkp <sgm4kv@gmail.com>
2017-12-14 15:38:09 +0000

    updated

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ sudo rm $path && sudo cp mkpasswd $path
 elif [ ! -e mkpasswd -a -e $path ]; then
 chmod 755 mkpasswd
 sudo cp mkpasswd $path
-#echo "mkpasswd is installed"
 else echo "cannot find mkpasswd"
 fi;;
 esac
