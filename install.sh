8b0543f6b4ba99b213d80893c5fc186543367cf8
looneytkp <sgm4kv@gmail.com>
2017-12-16 22:48:45 +0000

    updated and fix bugs

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 path=/usr/bin/mkpasswd
+path2=/usr/bin/db
+path3=/usr/bin
+shopt -u nocasematch
 case $1 in
-"") if [ -e mkpasswd -a -e $path ]; then
-chmod 755 mkpasswd
-sudo rm $path && sudo cp mkpasswd $path
-mkpasswd
-echo "mkpasswd is installed"
-elif [ -e mkpasswd -a ! -e $path ]; then
-chmod 755 mkpasswd
-sudo cp mkpasswd $path
-echo "mkpasswd is installed"
-mkpasswd
-else echo "cannot find mkpasswd"
+"") if [ -e mkpasswd -a -e db ]; then
+cat .changelogs
+read -p "Update ? [y/n]: " updt
+if [ $updt = y ]; then
+chmod 755 mkpasswd db
+sudo cp {mkpasswd,db} $path3
+else echo
+fi
+else "cannot find mkpasswd && db"
 fi;;
-"--update") if [ -e ~/.configfile/make-pass*/mkpasswd -a -e $path ]; then
-chmod 755 ~/.configfile/make-pass*/mkpasswd
-sudo rm $path && sudo cp ~/.configfile/make-pass*/mkpasswd $path
-#echo "mkpasswd is installed"
-elif [ ! -e ~/.configfile/make-pass*/mkpasswd -a -e $path ]; then
-chmod 755 ~/.configfile/make-pass*/mkpasswd
-sudo cp ~/.configfile/make-pass*/mkpasswd $path
-else echo "cannot find mkpasswd"
+"--update") if [ -e ~/.configfile/make-pass*/mkpasswd -a -e ~/.configfile/make-pass*/db ]; then
+chmod 755 ~/.configfile/make-pass*/mkpasswd db
+sudo cp ~/.configfile/make-pass*/{mkpasswd,db} $path3
 fi;;
 esac
